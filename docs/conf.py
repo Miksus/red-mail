457f84393152119d7c3e73a9f0087e96237330a8
Mikael Koli <koli.mikael@gmail.com>
2022-01-01 21:29:08 +0200

    docs: fix HTML title in docs

--- docs/conf.py
+++ docs/conf.py
@@ -69,7 +69,7 @@ html_theme_options = {
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_title = "Advanced Scheduler for Python"
+html_title = "Advanced Python Email Sending"
 html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
