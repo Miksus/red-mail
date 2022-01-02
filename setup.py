89792f820b098158ca487ced9da693774c3e84cb
Mikael Koli <koli.mikael@gmail.com>
2022-01-02 12:35:49 +0200

    pkg: updated PyPI classifiers

--- setup.py
+++ setup.py
@@ -15,12 +15,19 @@ setup(
     description="Email sending library",
     long_description=long_description,
     long_description_content_type="text/markdown",
-     classifiers=[
+    classifiers=[
+        "Topic :: Communications :: Email",
         "Operating System :: OS Independent",
 
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
 
         "Intended Audience :: Developers",
+        "Intended Audience :: Customer Service",
         "Intended Audience :: Financial and Insurance Industry",
      ],
      include_package_data=True, # for MANIFEST.in
