a290f6a70ffb1b0d2f67e70fb8d266af3582d1ad
Mikael Koli <koli.mikael@gmail.com>
2022-01-05 09:31:45 +0200

    test: better send test

--- redmail/test/email/test_send.py
+++ redmail/test/email/test_send.py
@@ -1,16 +1,38 @@
 
+from email.message import EmailMessage
 import pytest
 
 from redmail import EmailSender, send_email
 
+class MockServer:
+
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.is_login = False
+
+    def starttls(self):
+        return
+
+    def login(self, user=None, password=None):
+        self.is_login = True
+        return
+
+    def send_message(self, msg):
+        return
+
+    def quit(self):
+        return
+
 def test_send():
     email = EmailSender(host="localhost", port=0)
+    email._cls_smtp_server = MockServer
     # This should fail but we test everything else goes through
-    with pytest.raises(ConnectionRefusedError):
-        email.send(
-            subject="An example",
-            receivers=['koli.mikael@example.com']
-        )
+    msg = email.send(
+        subject="An example",
+        receivers=['koli.mikael@example.com']
+    )
+    assert isinstance(msg, EmailMessage)
 
 def test_send_function():
     # This should fail but we test everything else goes through
