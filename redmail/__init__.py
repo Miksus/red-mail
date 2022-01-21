921902e76d141202037418cca4a750acf110a1e5
Mikael Koli <koli.mikael@gmail.com>
2022-01-21 09:16:59 +0200

    add: pre-configured outlook

--- redmail/__init__.py
+++ redmail/__init__.py
@@ -1,3 +1,3 @@
-from .email import EmailSender, send_email, gmail
+from .email import EmailSender, send_email, gmail, outlook
 from . import _version
 __version__ = _version.get_versions()['version']
