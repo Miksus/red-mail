a509e16b11afdcb18f0cf6a55b2fe3aecd597722
Mikael Koli <koli.mikael@gmail.com>
2022-11-15 20:35:23 +0200

    setup.py: add Python 3.11 classifier

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
 
         "Intended Audience :: Developers",
         "Intended Audience :: Customer Service",
